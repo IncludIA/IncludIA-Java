ccb060c8e014ae7c1f4a88ff97d13dca88d418da
Lu1zEdu <le6269199@gmail.com>
2025-11-10 23:38:17 -0300

    colocando o Redis

--- src/main/java/com/fiap/gs2025/IncludIA_Java/IncludIaJavaApplication.java
+++ src/main/java/com/fiap/gs2025/IncludIA_Java/IncludIaJavaApplication.java
@@ -2,12 +2,14 @@ package com.fiap.gs2025.IncludIA_Java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching; // 1. IMPORTAR
 
 @SpringBootApplication
+@EnableCaching
 public class IncludIaJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IncludIaJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IncludIaJavaApplication.class, args);
+    }
 
 }
